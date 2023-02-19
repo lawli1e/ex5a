b30cad79746f5d6d8fa2e70f390ba90ed1d8ff7f
Pirapong <tristafire99@gmail.com>
2023-02-19 16:54:47 +0700

    refactor#1 adding validate function

--- common.js
+++ common.js
@@ -1,16 +1,25 @@
+function paceValidate(place){
+
+	//---------------------------
+	if(isNaN(pace)){
+		msg = "Please verify that the pace you have entered is correct!!!";
+		return {"status":0,"message":msg,"hour":0,"min":0};
+	}
+	if(!(pace >= 3 && pace <= 10)){
+		msg = "Running pace must be between 3 and 10 min/km";
+		return {"status":0,"message":msg,"hour":0,"min":0};
+	}
+
+}
+
 function calculate(p,d){
+
 		var msg = "";
 		var pace = parseFloat(p);
 		var dist = parseInt(d);
 	//---------------------------
-		if(isNaN(pace)){
-			msg = "Please verify that the pace you have entered is correct!!!";
-			return {"status":0,"message":msg,"hour":0,"min":0};
-		}
-		if(!(pace >= 3 && pace <= 10)){
-			msg = "Running pace must be between 3 and 10 min/km";
-			return {"status":0,"message":msg,"hour":0,"min":0};
-		}
+		var chk = paceValidate(pace);
+		if(chk != null)return chk;
 	//---------------------------
 		var min = dist * pace;
 		var hour = 0;
@@ -21,4 +30,5 @@ function calculate(p,d){
 	//---------------------------
 		msg = "successfully calculated!"
 		return {"status":1,"message":msg,"hour":hour,"min":min};
+
 }
